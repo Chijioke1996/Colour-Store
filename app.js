4e22ff353e6b39d71961637be1b6f7b92bb77535
unknown <ewuziejoke@gmail.com>
2024-08-24 15:49:22 +0100

    final  commit

--- app.js
+++ app.js
@@ -53,26 +53,3 @@ cart.addEventListener("click", () => {
 
 
 
-//CHECK OUT PAGE JAVASCRIPT
-
-const parent_container = document.querySelector('.parent-container');
-
-document.addEventListener("DOMContentLoaded", () => {
-
-    // Retrieve the cart items from localStorage
-    let cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
-
-    cartItems.forEach(item => {
-        const card = document.createElement('div');
-        card.classList.add('colour-card');
-
-        card.innerHTML = `
-            <div class="colour-frame ${item.colorClass}"></div>
-            <p class="colour-class">${item.colourClassText}</p>
-            <p class="colour-name">${item.colourName}</p>
-            <p class="price">${item.price}</p>
-        `;
-
-        parent_container.appendChild(card);
-    });
-});
